498bd96251cc5234ddaf11f494f4a46ec01f6348
Karl Keefer <karl@vsco.co>
2017-05-19 11:14:13 -0700

    use curl instead of file_get_contents for shorter timeouts

--- src/Klaviyo.php
+++ src/Klaviyo.php
@@ -61,7 +61,13 @@ class Klaviyo {
 
     protected function make_request($path, $params) {
         $url = $this->host . $path . '?' . $params;
-        $response = file_get_contents($url);
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $url,
+            CURLOPT_TIMEOUT => 5 // seconds
+        ));
+        $response = curl_exec();
         return $response == '1';
     }
 };
